4d8c5238e71ee5111df312c470e7facd65669b64
Alexander Soong <alexsoong91@gmail.com>
2021-01-05 18:25:09 -0800

    December distribution rewards

---
+++ deployments/utils/deploys/rewards/dec20Distribution.ts
@@ -0,0 +1,113 @@
+import { ether } from "@utils/index";
+import { DistributionFormat } from "@utils/types";
+
+export const DECEMBER_MERKLE_DISTRIBUTION = [
+  {
+    "address": "0xfa7513021002f7181333b773bf801dfd7fd91ce7",
+    "earnings": ether(982.32),
+  },
+  {
+    "address": "0xB188dADDde4df91fD4CC4AADC5334873099D4B94",
+    "earnings": ether(982.32),
+  },
+  {
+    "address": "0xD425333DD22B049f143522e519a06462c633F37a",
+    "earnings": ether(982.32),
+  },
+  {
+    "address": "0x69238aF5756617E5218810057A03da509ec51fd4",
+    "earnings": ether(785.85),
+  },
+  {
+    "address": "0xf6401adc23faa6b9ad83ea8604ca7254cb7f53e7",
+    "earnings": ether(589.39),
+  },
+  {
+    "address": "0x5CD4EF55C339ef01f79f494c0a568df90699Aa22",
+    "earnings": ether(739.39),
+  },
+  {
+    "address": "0x9A6c02Eb5F511EBf2dFb4309F4b30cd5d02a44F6",
+    "earnings": ether(491.16),
+  },
+  {
+    "address": "0xcffe08bdf20918007f8ab268c32f8756494fc8d8",
+    "earnings": ether(491.16),
+  },
+  {
+    "address": "0x6B1050C1C6B288C79Ac1db299Dc481048aBBBbcD",
+    "earnings": ether(294.70),
+  },
+  {
+    "address": "0x1a171a91B4Aa1A669e2397D6670746DDcDd4fbBe",
+    "earnings": ether(294.70),
+  },
+  {
+    "address": "0x026FD30023D9199450883B3c7f82b7C9ebbEB2F7",
+    "earnings": ether(196.46),
+  },
+  {
+    "address": "0xA386621F99D2B74DE33051cd5A5d00967668afDd",
+    "earnings": ether(196.46),
+  },
+  {
+    "address": "0x4402D581Ee5bCd64BD150Ec53D17D13f7AcCc9A4",
+    "earnings": ether(98.23),
+  },
+  {
+    "address": "0xbdac5657eDd13F47C3DD924eAa36Cf1Ec49672cc",
+    "earnings": ether(98.23),
+  },
+  {
+    "address": "0xCed2f0dcb70757339F7072A3ffA08a4A73497435",
+    "earnings": ether(49.12),
+  },
+  {
+    "address": "0x8062E69aF89430d0749697ceDE9614Cda80325e0",
+    "earnings": ether(49.12),
+  },
+  {
+    "address": "0xfa38ac83a796169a0882d999468f2dc01cbaa121",
+    "earnings": ether(49.12),
+  },
+  {
+    "address": "0x833C68CcF99F7c6A3728Ee40508Cc2EaFD38263e",
+    "earnings": ether(39.29),
+  },
+  {
+    "address": "0x62661b101ca48734668669a9f1cb83c4889049f3",
+    "earnings": ether(39.29),
+  },
+  {
+    "address": "0xa518006Abfe57d02C51141C5CD169F8a2BbcDDc5",
+    "earnings": ether(39.29),
+  },
+  {
+    "address": "0xFeC80D2993970D9C821AFC5073d506A9f02cF4DB",
+    "earnings": ether(29.65),
+  },
+  {
+    "address": "0x9aADd82015413781D4b030aB839E965b1Ae0b264",
+    "earnings": ether(9.82),
+  },
+  {
+    "address": "0xa14e3Ec7e50091C5eCC3F1B76096210B01daaaC8",
+    "earnings": ether(9.82),
+  },
+  {
+    "address": "0xB9A233A7348a596ce270315FcFDcF8E0c677ACD8",
+    "earnings": ether(9.82),
+  },
+  {
+    "address": "0x381B58845271009b3D147EBB70D6d5585091840F",
+    "earnings": ether(150),
+  },
+  {
+    "address": "0x349A59baef8e532B09767c833632F37917aBD058",
+    "earnings": ether(10),
+  },
+  {
+    "address": "0x064f155a887810006B05De59358b3cDF06db0644",
+    "earnings": ether(10),
+  },
+] as DistributionFormat[];
---
+++ test/deploys/004_dec_20_coop_rewards.spec.ts
@@ -0,0 +1,51 @@
+import "module-alias/register";
+import { deployments } from "@nomiclabs/buidler";
+
+import { Account } from "@utils/types";
+import { MerkleDistributor } from "@deployments/utils/contracts/index";
+import {
+  addSnapshotBeforeRestoreAfterEach,
+  getAccounts,
+  getWaffleExpect,
+} from "@utils/index";
+import {
+  getContractAddress,
+} from "@deployments/utils/deploys/outputHelper";
+import { DECEMBER_MERKLE_DISTRIBUTION } from "@deployments/utils/deploys/rewards/dec20Distribution";
+
+import { MerkleDistributorFactory } from "@setprotocol/index-coop-contracts/dist/typechain/MerkleDistributorFactory";
+
+const expect = getWaffleExpect();
+
+describe("RewardsDec20MerkleDistributor", () => {
+  let deployer: Account;
+
+  let distributorContractInstance: MerkleDistributor;
+
+  before(async () => {
+    [
+      deployer,
+    ] = await getAccounts();
+
+    await deployments.fixture();
+
+    const deployedMerkleDistributorContract = await getContractAddress("RewardsDec20MerkleDistributor");
+    distributorContractInstance = new MerkleDistributorFactory(deployer.wallet).attach(deployedMerkleDistributorContract);
+  });
+
+  addSnapshotBeforeRestoreAfterEach();
+
+  describe("RewardsDec20MerkleDistributor", async () => {
+    it("should have the correct token address", async () => {
+      const indexToken = await distributorContractInstance.token();
+      expect(indexToken).to.eq(await getContractAddress("IndexToken"));
+    });
+
+    it("should have the correct unclaimed", async () => {
+      for (let rootIndex = 0; rootIndex < DECEMBER_MERKLE_DISTRIBUTION.length; rootIndex++) {
+        const isClaimed = await distributorContractInstance.isClaimed(rootIndex);
+        expect(isClaimed).to.eq(false);
+      }
+    });
+  });
+});
