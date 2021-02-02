30773b10fa2d70e87af4f2416948e2f6eee659ce
Alexander Soong <alexsoong91@gmail.com>
2021-02-02 13:15:31 -0800

    Deploy January distributor rewards

---
+++ deployments/utils/deploys/rewards/jan21Distribution.ts
@@ -0,0 +1,133 @@
+import { ether } from "@utils/index";
+import { DistributionFormat } from "@utils/types";
+
+export const JANUARY_MERKLE_DISTRIBUTION = [
+  {
+    "address": "0xB188dADDde4df91fD4CC4AADC5334873099D4B94",
+    "earnings": ether(598.21),
+  },
+  {
+    "address": "0x69238aF5756617E5218810057A03da509ec51fd4",
+    "earnings": ether(843.00),
+  },
+  {
+    "address": "0x5CD4EF55C339ef01f79f494c0a568df90699Aa22",
+    "earnings": ether(598.21),
+  },
+  {
+    "address": "0xD425333DD22B049f143522e519a06462c633F37a",
+    "earnings": ether(843.00),
+  },
+  {
+    "address": "0xfa7513021002f7181333b773bf801dfd7fd91ce7",
+    "earnings": ether(398.80),
+  },
+  {
+    "address": "0xf6401adc23faa6b9ad83ea8604ca7254cb7f53e7",
+    "earnings": ether(448.95),
+  },
+  {
+    "address": "0xA386621F99D2B74DE33051cd5A5d00967668afDd",
+    "earnings": ether(299.10),
+  },
+  {
+    "address": "0x1a171a91B4Aa1A669e2397D6670746DDcDd4fbBe",
+    "earnings": ether(249.25),
+  },
+  {
+    "address": "0x026FD30023D9199450883B3c7f82b7C9ebbEB2F7",
+    "earnings": ether(249.25),
+  },
+  {
+    "address": "0xcffe08bdf20918007f8ab268c32f8756494fc8d8",
+    "earnings": ether(249.25),
+  },
+  {
+    "address": "0xEF60A333D909f97CA9F6248B831f979bf5e205D3",
+    "earnings": ether(199.40),
+  },
+  {
+    "address": "0xfa38ac83a796169a0882d999468f2dc01cbaa121",
+    "earnings": ether(149.55),
+  },
+  {
+    "address": "0xFeC80D2993970D9C821AFC5073d506A9f02cF4DB",
+    "earnings": ether(159.55),
+  },
+  {
+    "address": "0xc290Cfb8D020c0615e9C63036f4319Cc41717E68",
+    "earnings": ether(149.55),
+  },
+  {
+    "address": "0xB20A34F937871F0CDe4D6f7a79725bF52C8C5239",
+    "earnings": ether(131.38),
+  },
+  {
+    "address": "0xA32E9A93407C629D1a952bdAdc0D2f72887d0326",
+    "earnings": ether(199.70),
+  },
+  {
+    "address": "0x4402D581Ee5bCd64BD150Ec53D17D13f7AcCc9A4",
+    "earnings": ether(99.70),
+  },
+  {
+    "address": "0x0d896d05447Bd344Ef773552F4D0408718F3e054",
+    "earnings": ether(99.85),
+  },
+  {
+    "address": "0xfeb69beBa180C5f4747FA426518326571E4C893D",
+    "earnings": ether(49.85),
+  },
+  {
+    "address": "0x381B58845271009b3D147EBB70D6d5585091840F",
+    "earnings": ether(49.85),
+  },
+  {
+    "address": "0xd5D6242177279596B2672B9110c0771eD2E941E6",
+    "earnings": ether(89.14),
+  },
+  {
+    "address": "0xc571aba407d0771Bc0eceB318655B18834e1ea6F",
+    "earnings": ether(24.93),
+  },
+  {
+    "address": "0xd213f746d52002E3e84517790603018D7fCd2CeA",
+    "earnings": ether(24.93),
+  },
+  {
+    "address": "0xe0C80Cf09fbdCD6285a455686BAd247C9C02Ec88",
+    "earnings": ether(24.93),
+  },
+  {
+    "address": "0xa921789802E13243C6827D37526Ce632eF4A138b",
+    "earnings": ether(24.93),
+  },
+  {
+    "address": "0x0eD1d2b2d14E3b798759D62b9e4a587b233D60C9",
+    "earnings": ether(9.97),
+  },
+  {
+    "address": "0xa14e3Ec7e50091C5eCC3F1B76096210B01daaaC8",
+    "earnings": ether(9.97),
+  },
+  {
+    "address": "0x8062E69aF89430d0749697ceDE9614Cda80325e0",
+    "earnings": ether(9.97),
+  },
+  {
+    "address": "0xCed2f0dcb70757339F7072A3ffA08a4A73497435",
+    "earnings": ether(9.97),
+  },
+  {
+    "address": "0xB9A233A7348a596ce270315FcFDcF8E0c677ACD8",
+    "earnings": ether(9.97),
+  },
+  {
+    "address": "0x62661b101ca48734668669a9f1cb83c4889049f3",
+    "earnings": ether(9.97),
+  },
+  {
+    "address": "0x3136111d944950503e26a7b9fe7b2cc62ab5912e",
+    "earnings": ether(10),
+  }
+] as DistributionFormat[];
