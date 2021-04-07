2861ce1b76d3595fac3b6ce6056abd7736de4bd3
Alexander Soong <alexsoong91@gmail.com>
2021-04-06 23:31:04 -0700

    March 2021 Reward Distributor

--- deployments/utils/deploys/rewards/index.ts
+++ deployments/utils/deploys/rewards/index.ts
@@ -2,5 +2,4 @@ export * from "./001_nov20Distribution";
 export * from "./002_dec20Distribution";
 export * from "./003_jan21Distribution";
 export * from "./004_feb21Distribution";
-
-
+export * from "./005_mar21Distribution";
---
+++ test/deploys/010_mar_21_coop_rewards.spec.ts
@@ -0,0 +1,52 @@
+import "module-alias/register";
+import { deployments } from "hardhat";
+
+import { Account } from "@utils/types";
+import {
+  MerkleDistributor,
+  MerkleDistributor__factory
+} from "@set/typechain/index";
+import {
+  addSnapshotBeforeRestoreAfterEach,
+  getAccounts,
+  getWaffleExpect,
+} from "@utils/index";
+import {
+  getContractAddress,
+  MARCH_MERKLE_DISTRIBUTION,
+} from "@deployments/utils";
+
+const expect = getWaffleExpect();
+
+describe("RewardsMar21MerkleDistributor", () => {
+  let deployer: Account;
+
+  let distributorContractInstance: MerkleDistributor;
+
+  before(async () => {
+    [
+      deployer,
+    ] = await getAccounts();
+
+    await deployments.fixture();
+
+    const deployedMerkleDistributorContract = await getContractAddress("RewardsMar21MerkleDistributor");
+    distributorContractInstance = new MerkleDistributor__factory(deployer.wallet).attach(deployedMerkleDistributorContract);
+  });
+
+  addSnapshotBeforeRestoreAfterEach();
+
+  describe("RewardsMar21MerkleDistributor", async () => {
+    it("should have the correct token address", async () => {
+      const indexToken = await distributorContractInstance.token();
+      expect(indexToken).to.eq(await getContractAddress("IndexToken"));
+    });
+
+    it("should have the correct unclaimed", async () => {
+      for (let rootIndex = 0; rootIndex < MARCH_MERKLE_DISTRIBUTION.length; rootIndex++) {
+        const isClaimed = await distributorContractInstance.isClaimed(rootIndex);
+        expect(isClaimed).to.eq(false);
+      }
+    });
+  });
+});
