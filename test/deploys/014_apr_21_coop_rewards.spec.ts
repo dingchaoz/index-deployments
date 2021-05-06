9afa9ee317dbe25ca2f40b44be0564923e12d3fc
Dylan Tran <dylan.ad.tran@gmail.com>
2021-05-06 08:52:15 -0700

    add tests for apr rewards deploy

---
+++ test/deploys/014_apr_21_coop_rewards.spec.ts
@@ -0,0 +1,52 @@
+import "module-alias/register";
+import { deployments } from "hardhat";
+
+import { Account } from "@utils/types";
+import {
+  MerkleDistributor,
+  MerkleDistributor__factory
+} from "@set/typechain/index";
+import {
+  addSnapshotBeforeRestoreAfterEach,
+  getAccounts,
+  getWaffleExpect,
+} from "@utils/index";
+import {
+  getContractAddress,
+  APRIL_MERKLE_DISTRIBUTION,
+} from "@deployments/utils";
+
+const expect = getWaffleExpect();
+
+describe("RewardsApr21MerkleDistributor", () => {
+  let deployer: Account;
+
+  let distributorContractInstance: MerkleDistributor;
+
+  before(async () => {
+    [
+      deployer,
+    ] = await getAccounts();
+
+    await deployments.fixture();
+
+    const deployedMerkleDistributorContract = await getContractAddress("RewardsApr21MerkleDistributor");
+    distributorContractInstance = new MerkleDistributor__factory(deployer.wallet).attach(deployedMerkleDistributorContract);
+  });
+
+  addSnapshotBeforeRestoreAfterEach();
+
+  describe("RewardsApr21MerkleDistributor", async () => {
+    it("should have the correct token address", async () => {
+      const indexToken = await distributorContractInstance.token();
+      expect(indexToken).to.eq(await getContractAddress("IndexToken"));
+    });
+
+    it("should have the correct unclaimed", async () => {
+      for (let rootIndex = 0; rootIndex < APRIL_MERKLE_DISTRIBUTION.length; rootIndex++) {
+        const isClaimed = await distributorContractInstance.isClaimed(rootIndex);
+        expect(isClaimed).to.eq(false);
+      }
+    });
+  });
+});
